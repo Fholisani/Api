52ef096e39176c35dc8172e3a4a539427ae3fe66
Fholisani.Mashegana <mashegana@gmail.com>
2017-08-26 01:51:07 +0200

    Updated app to use war file

--- src/main/java/com/mydevgeek/Fholisani/FholisaniApplication.java
+++ src/main/java/com/mydevgeek/Fholisani/FholisaniApplication.java
@@ -5,13 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.mydevgeek")
 @EnableJpaRepositories(basePackages = "com.mydevgeek.repo")
 @EntityScan(basePackages = "com.mydevgeek.domain")
-public class FholisaniApplication {
+public class FholisaniApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FholisaniApplication .class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FholisaniApplication.class, args);
 	}
